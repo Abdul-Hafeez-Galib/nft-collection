14afec8d28bb422f617efd4b30795320b37e180c
Abdul-Hafeez-Galib <galibabdulhafeez@gmail.com>
2022-11-18 18:51:07 +0530

    app is done

---
+++ smart_contract/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x53e5bb18b9a23cce50e1383a50239f68996a3064";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
+
+// DEPLOYED CONTRACT: 0xcD1f807EF9EA0E6621e997ebB2952902827be235
