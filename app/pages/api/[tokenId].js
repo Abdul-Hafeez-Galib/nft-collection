753f014fa5c0bc127b8307dd89e0912490ea1119
Abdul-Hafeez-Galib <galibabdulhafeez@gmail.com>
2022-11-18 19:09:13 +0530

    resolved error in deployment

---
+++ app/pages/api/[tokenId].js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  const tokenId = req.query.tokenId;
+
+  const image_url =
+    "https://raw.githubusercontent.com/Abdul-Hafeez-Galib/nft-collection/main/app/public/cryptodevs/";
+
+  res.status(200).json({
+    name: "Crypto Dev #" + tokenId,
+    description: "Crypto Dev is a collection of developers in crypto",
+    image: image_url + tokenId + ".svg",
+  });
+}
